f4b1196ea8ac25c8ca6f093761679df02c1d4a32
Nick Hardman <kp22738@bristol.ac.uk>
2024-11-23 17:12:11 +0000

    added functions to find the furthest and closest distances to another particle for the 100 random coordinates, and created csv recording these values. Also calculates the average for both of these values and outputs into the terminal

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <cassert>
 #include "rng.h"
+#include <cmath>
+#include <limits>
 
 struct point 
 {
@@ -10,6 +12,11 @@ struct point
 };
 
 using utils::rand::randDouble;
+
+double distancecalc(const point& a, const point&b)
+{
+    return std::sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+}
 
 int main() 
 {
@@ -28,13 +35,52 @@ int main()
 
     }
 
-    std::ofstream out("particle_points_unoptimised_unparralisd.csv");
+//    std::ofstream out("particle_points_unoptimised_unparralisd.csv");
 
-    for (int i=0; i<data.size(); ++i)
+//    for (int i=0; i<data.size(); ++i)
+//    {
+//        out << data[i].x << "," << data[i].y << std::endl;
+//    }
+//    out.close();
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::min());
+    double closest_total=0.0, furthest_total=0.0;
+
+    for (int i=0; i<N; ++i)
     {
-        out << data[i].x << "," << data[i].y << std::endl;
+        for (int j=0; j<N; ++j)
+        {
+            if (i==j) continue;
+            double dist = distancecalc(data[i], data[j]);
+            closestdist[i]=std::min(closestdist[i], dist);
+            furthestdist[i]=std::max(furthestdist[i], dist);
+        }
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
     }
-    out.close();
+
+    std::ofstream close("closest_distances_unparallelised_unoptimised.csv");
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::ofstream far("furthest_distances_unparallelised_unoptimised.csv");
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close();
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+
 
     return EXIT_SUCCESS;
 }
