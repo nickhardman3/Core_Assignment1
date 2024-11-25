c09bf256afbdc6c62df9f7c21c2d14d575db6533
Nick Hardman <kp22738@bristol.ac.uk>
2024-11-25 21:52:02 +0000

    added optimised calculations for non-parallelised, however parallelised functions outlined, but not complete

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -324,7 +324,7 @@ void parallelrandompoints(int N)
 
     double startTime = omp_get_wtime();
 
-    #pragma omp parallel for reduction(+:closest_total,furthest_total)
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(static, 3)
     for (int i=0; i<N; ++i)
     {
         double local_closest = std::numeric_limits<double>::max();
@@ -581,13 +581,72 @@ void parallelwrapcsvpoints(int N)
     std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
 }
 
-int main()
+void optimisedrandompoints(int N)
 {
-    std::cout << "How many threads? (1-8)";
-    int threads;
-    std::cin >> threads;
-    omp_set_num_threads(threads);
-    
+    std::vector<point> data;
+    data.reserve(N);
+
+    utils::rand::seedRand(324255342);
+
+    for (int i=0; i<N; ++i) 
+    {
+        double x = utils::rand::randDouble(0.0, 1.0);
+        double y = utils::rand::randDouble(0.0, 1.0);
+
+        data.push_back({x, y});
+
+    }
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    for (int i=0; i<N; ++i)
+    {
+        for (int j=i+1; j<N; ++j)
+        {
+            double dist = distancecalc(data[i], data[j]);
+
+            closestdist[i]=std::min(closestdist[i], dist);
+            furthestdist[i]=std::max(furthestdist[i], dist);
+
+            closestdist[j]=std::min(closestdist[j], dist);
+            furthestdist[j]=std::max(furthestdist[j], dist);            
+        }
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string close_random = std::to_string(N) + "closest_distances_unparallelised_optimised.csv";
+    std::ofstream close(close_random);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string far_random = std::to_string(N) + "furthest_distances_unparallelised_optimised.csv";
+    std::ofstream far(far_random);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close();
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;    
+}
+
+int main()
+{ 
     std::cout << "Choose input type (1 = Random object locations, 2 = CSV object locations)";
     int choice;
     std::cin >> choice;
@@ -622,6 +681,11 @@ int main()
                 }
                 else if (para == 2)
                 {
+                    std::cout << "How many threads? (1-8)";
+                    int threads;
+                    std::cin >> threads;
+                    omp_set_num_threads(threads);
+
                     if (calc == 1)
                     {
                         parallelrandompoints(N);
@@ -676,6 +740,11 @@ int main()
                 }
                 else if (para == 2)
                 {
+                    std::cout << "How many threads? (1-8)";
+                    int threads;
+                    std::cin >> threads;
+                    omp_set_num_threads(threads);
+
                     if (calc == 1)
                     {
                         parallelcsvpoints(N);
@@ -697,10 +766,16 @@ int main()
         }
         else
         {
-            std::cerr << "Invalid number of locations\n";
+            std::cerr << "Invalid choice";
         }        
     }
-
+    else if (choice == 3)
+    {
+        std::cout << "100000 or 200000 object locations?";
+        int N;
+        std::cin >> N;
+        optimisedrandompoints(N);
+    }
     else
     {
         std::cerr << "Invalid choice";
