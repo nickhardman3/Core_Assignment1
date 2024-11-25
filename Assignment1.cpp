8bc36022d8bee5e2d173ce1dde10dc950137d9b2
Nick Hardman <kp22738@bristol.ac.uk>
2024-11-25 21:52:19 +0000

    added optimised calculations for non-parallelised, however parallelised functions outlined, but not complete

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -324,7 +324,7 @@ void parallelrandompoints(int N)
 
     double startTime = omp_get_wtime();
 
-    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(static, 3)
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
     for (int i=0; i<N; ++i)
     {
         double local_closest = std::numeric_limits<double>::max();
@@ -394,7 +394,7 @@ void parallelcsvpoints(int N)
 
     double startTime = omp_get_wtime();
 
-    #pragma omp parallel for reduction(+:closest_total,furthest_total)
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
     for (int i=0; i<N; ++i)
     {
         double local_closest = std::numeric_limits<double>::max();
@@ -463,7 +463,7 @@ void parallelwraprandompoints(int N)
 
     double startTime = omp_get_wtime();
 
-    #pragma omp parallel for reduction(+:closest_total,furthest_total)
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
     for (int i=0; i<N; ++i)
     {
         double local_closest = std::numeric_limits<double>::max();
@@ -534,7 +534,7 @@ void parallelwrapcsvpoints(int N)
 
     double startTime = omp_get_wtime();
 
-    #pragma omp parallel for reduction(+:closest_total,furthest_total)
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
     for (int i=0; i<N; ++i)
     {
         double local_closest = std::numeric_limits<double>::max();
@@ -644,6 +644,484 @@ void optimisedrandompoints(int N)
     std::cout << "Average distance to the closest object: " << close_avg << std::endl;
     std::cout << "Average distance to the furthest object: " << far_avg << std::endl;    
 }
+
+void optimisedcsvpoints(int N)
+{
+    std::vector<point> data;
+    std::string csv = std::to_string(N) + " locations.csv";
+    std::ifstream infile(csv);
+    std::vector<std::vector<double>> raw_data;
+    readcsv(infile, raw_data);
+    infile.close();
+
+    for (const auto& row: raw_data)
+    {
+        if (row.size () >=2)
+        {
+            data.push_back({static_cast<double>(row[0]), static_cast<double>(row[1])});
+        }
+    }
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    for (int i=0; i<N; ++i)
+    {
+        for (int j=i+1; j<N; ++j)
+        {
+            double dist = distancecalc(data[i], data[j]);
+
+            closestdist[i]=std::min(closestdist[i], dist);
+            furthestdist[i]=std::max(furthestdist[i], dist);
+
+            closestdist[j]=std::min(closestdist[j], dist);
+            furthestdist[j]=std::max(furthestdist[j], dist);            
+        }
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string csv_close = std::to_string(N) + "csv_closest_distances_unparallelised_optimised.csv";
+    std::ofstream close(csv_close);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string csv_far = std::to_string(N) + "csv_furthest_distances_unparallelised_optimised.csv";
+    std::ofstream far(csv_far);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close(); 
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+}
+
+void optimisedwraprandompoints(int N)
+{
+    std::vector<point> data;
+    data.reserve(N);
+
+    utils::rand::seedRand(324255342);
+
+    for (int i=0; i<N; ++i) 
+    {
+        double x = utils::rand::randDouble(0.0, 1.0);
+        double y = utils::rand::randDouble(0.0, 1.0);
+
+        data.push_back({x, y});
+
+    }
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    for (int i=0; i<N; ++i)
+    {
+        for (int j=i+1; j<N; ++j)
+        {
+            double dist = distancecalc(data[i], data[j]);
+
+            closestdist[i]=std::min(closestdist[i], dist);
+            furthestdist[i]=std::max(furthestdist[i], dist);
+
+            closestdist[j]=std::min(closestdist[j], dist);
+            furthestdist[j]=std::max(furthestdist[j], dist);            
+        }
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string close_random = std::to_string(N) + "closest_distances_unparallelised_wrap_optimised.csv";
+    std::ofstream close(close_random);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string far_random = std::to_string(N) + "furthest_distances_unparallelised_wrap_optimised.csv";
+    std::ofstream far(far_random);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close();
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+
+}
+
+void optimisedwrapcsvpoints(int N)
+{
+    std::vector<point> data;
+    std::string csv = std::to_string(N) + " locations.csv";
+    std::ifstream infile(csv);
+    std::vector<std::vector<double>> raw_data;
+    readcsv(infile, raw_data);
+    infile.close();
+
+    for (const auto& row: raw_data)
+    {
+        if (row.size () >=2)
+        {
+            data.push_back({static_cast<double>(row[0]), static_cast<double>(row[1])});
+        }
+    }
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    for (int i=0; i<N; ++i)
+    {
+        for (int j=i+1; j<N; ++j)
+        {
+            double dist = distancecalc(data[i], data[j]);
+
+            closestdist[i]=std::min(closestdist[i], dist);
+            furthestdist[i]=std::max(furthestdist[i], dist);
+
+            closestdist[j]=std::min(closestdist[j], dist);
+            furthestdist[j]=std::max(furthestdist[j], dist);            
+        }
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string csv_close = std::to_string(N) + "csv_closest_distances_unparallelised_wrap_optimised.csv";
+    std::ofstream close(csv_close);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string csv_far = std::to_string(N) + "csv_furthest_distances_unparallelised_wrap_optimised.csv";
+    std::ofstream far(csv_far);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close(); 
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+}
+
+void optimisedparallelrandompoints(int N) 
+{
+    std::vector<point> data;
+    data.reserve(N);
+
+    utils::rand::seedRand(324255342);
+
+    for (int i=0; i<N; ++i) 
+    {
+        double x = utils::rand::randDouble(0.0, 1.0);
+        double y = utils::rand::randDouble(0.0, 1.0);
+
+        data.push_back({x, y});
+
+    }
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
+    for (int i=0; i<N; ++i)
+    {
+        double local_closest = std::numeric_limits<double>::max();
+        double local_furthest = std::numeric_limits<double>::lowest();
+
+        for (int j=0; j<N; ++j)
+        {
+            if (i==j) continue;
+            double dist = wrapdistance(data[i], data[j]);
+            
+            local_closest = std::min(local_closest, dist);
+            local_furthest = std::max(local_furthest, dist);
+        }
+        closestdist[i] = local_closest;
+        furthestdist[i] = local_furthest;
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string close_random_para = std::to_string(N) + "closest_distances_parallelised_unoptimised.csv";
+    std::ofstream close(close_random_para);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string far_random_para = std::to_string(N) + "furthest_distances_parallelised_unoptimised.csv";
+    std::ofstream far(far_random_para);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close();
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+}
+
+void optimisedparallelcsvpoints(int N)
+{
+    std::vector<point> data;
+    std::string csv = std::to_string(N) + " locations.csv";
+    std::ifstream infile(csv);
+    std::vector<std::vector<double>> raw_data;
+    readcsv(infile, raw_data);
+    infile.close();
+
+    for (const auto& row: raw_data)
+    {
+        if (row.size () >=2)
+        {
+            data.push_back({static_cast<double>(row[0]), static_cast<double>(row[1])});
+        }
+    }
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
+    for (int i=0; i<N; ++i)
+    {
+        double local_closest = std::numeric_limits<double>::max();
+        double local_furthest = std::numeric_limits<double>::lowest();
+
+        for (int j=0; j<N; ++j)
+        {
+            if (i==j) continue;
+            double dist = wrapdistance(data[i], data[j]);
+            
+            local_closest = std::min(local_closest, dist);
+            local_furthest = std::max(local_furthest, dist);
+        }
+        closestdist[i] = local_closest;
+        furthestdist[i] = local_furthest;
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string csv_close_para = std::to_string(N) + "csv_closest_distances_parallelised_unoptimised.csv";
+    std::ofstream close(csv_close_para);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string csv_far_para = std::to_string(N) + "csv_furthest_distances_parallelised_unoptimised.csv";
+    std::ofstream far(csv_far_para);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close(); 
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+}
+
+void optimisedparallelwraprandompoints(int N)
+{
+    std::vector<point> data;
+    data.reserve(N);
+
+    utils::rand::seedRand(324255342);
+
+    for (int i=0; i<N; ++i) 
+    {
+        double x = utils::rand::randDouble(0.0, 1.0);
+        double y = utils::rand::randDouble(0.0, 1.0);
+
+        data.push_back({x, y});
+
+    }
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
+    for (int i=0; i<N; ++i)
+    {
+        double local_closest = std::numeric_limits<double>::max();
+        double local_furthest = std::numeric_limits<double>::lowest();
+
+        for (int j=0; j<N; ++j)
+        {
+            if (i==j) continue;
+            double dist = wrapdistance(data[i], data[j]);
+            
+            local_closest = std::min(local_closest, dist);
+            local_furthest = std::max(local_furthest, dist);
+        }
+        closestdist[i] = local_closest;
+        furthestdist[i] = local_furthest;
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string close_random_para = std::to_string(N) + "closest_distances_parallelised_wrap.csv";
+    std::ofstream close(close_random_para);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string far_random_para = std::to_string(N) + "furthest_distances_parallelised_wrap.csv";
+    std::ofstream far(far_random_para);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close();
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+
+}
+
+void optimisedparallelwrapcsvpoints(int N)
+{
+    std::vector<point> data;
+    std::string csv = std::to_string(N) + " locations.csv";
+    std::ifstream infile(csv);
+    std::vector<std::vector<double>> raw_data;
+    readcsv(infile, raw_data);
+    infile.close();
+
+    for (const auto& row: raw_data)
+    {
+        if (row.size () >=2)
+        {
+            data.push_back({static_cast<double>(row[0]), static_cast<double>(row[1])});
+        }
+    }
+
+    std::vector<double> closestdist(N, std::numeric_limits<double>::max());
+    std::vector<double> furthestdist(N, std::numeric_limits<double>::lowest());
+    double closest_total=0.0, furthest_total=0.0;
+
+    double startTime = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+:closest_total,furthest_total) schedule(dynamic, 2)
+    for (int i=0; i<N; ++i)
+    {
+        double local_closest = std::numeric_limits<double>::max();
+        double local_furthest = std::numeric_limits<double>::lowest();
+
+        for (int j=0; j<N; ++j)
+        {
+            if (i==j) continue;
+            double dist = wrapdistance(data[i], data[j]);
+            
+            local_closest = std::min(local_closest, dist);
+            local_furthest = std::max(local_furthest, dist);
+        }
+        closestdist[i] = local_closest;
+        furthestdist[i] = local_furthest;
+
+        closest_total+=closestdist[i];
+        furthest_total+=furthestdist[i];
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    std::string csv_close = std::to_string(N) + "csv_closest_distances_parallelised_wrap.csv";
+    std::ofstream close(csv_close);
+    for (int i=0; i<N; ++i)
+    {
+        close << closestdist[i] << std::endl;
+    }
+    close.close();
+
+    std::string csv_far = std::to_string(N) + "csv_furthest_distances_parallelised_wrap.csv";
+    std::ofstream far(csv_far);
+    for (int i=0; i<N; ++i)
+    {
+        far << furthestdist[i] << std::endl;
+    }
+    far.close(); 
+
+    double close_avg = closest_total/N;
+    double far_avg = furthest_total/N;
+
+    std::cout << "Elapsed time: " << elapsedTime << "seconds" << std::endl;
+    std::cout << "Average distance to the closest object: " << close_avg << std::endl;
+    std::cout << "Average distance to the furthest object: " << far_avg << std::endl;
+}
+
 
 int main()
 { 
